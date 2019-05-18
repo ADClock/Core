2223f446228f5454bdbe710b01b41e0cca458691
Paul Maximilian Krüger <paul.krueger@alumni.fh-aachen.de>
2019-05-18 18:21:07 +0200

    Added simple data communication

--- Code/ADClockArduino/src/main.cpp
+++ Code/ADClockArduino/src/main.cpp
@@ -1,11 +1,23 @@
 #include <Arduino.h>
+#include "DataCom.h"
+
+#define DATA_INTERRUPT 2
 
 void setup()
 {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  DataCom::init();
+
+  // Interrupt für Datenempfang
+  pinMode(DATA_INTERRUPT, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(DATA_INTERRUPT), DataCom::reciveData, CHANGE);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
+  Serial.println("Sending data..");
+  DataCom::sendData(42);
+  delay(1000);
 }
